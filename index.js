b99a8c69108d9b58da511a54696c658bfcdd1e0e
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-29 21:14:39 +0600

    database connection

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//database
+
+// user: dbUser1
+// pass: kcn9Y4jMLFEuWIGb
+
+const uri =
+  "mongodb+srv://dbuser2:kM1RkIa6JfPWkwg1@cluster0.mj0nqa8.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollections = client.db("NodeMongoCrud").collection("users");
+    const user = { name: "dabasish", email: "heldfkl@gmail.com" };
+
+    const result = await userCollections.insertOne(user);
+    console.log(result);
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Mongo crud app is running");
